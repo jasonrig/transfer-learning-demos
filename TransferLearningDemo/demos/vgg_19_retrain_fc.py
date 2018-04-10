8069999ad61b79e07235563e6cf61342c6095447
Jason Rigby <jason.rigby@monash.edu>
2018-04-10 20:12:30 +1000

    working on demo codes

--- TransferLearningDemo/demos/vgg_19_retrain_fc.py
+++ TransferLearningDemo/demos/vgg_19_retrain_fc.py
@@ -78,8 +78,8 @@ def input_fn(test=False, batch_size=100, epochs=1):
 def input_fn_predict(file_names):
     def _input_fn():
         def decode_img(file_content):
-            img = tf.image.resize_images([tf.image.decode_image(file_content)], (224,224))
-            img.set_shape((1,224,224,3))
+            img = tf.image.decode_jpeg(file_content)
+            img = tf.image.resize_images([img], (224,224))
             return img
 
         dataset = tf.data.Dataset.from_tensor_slices(tf.constant(file_names, tf.string))
