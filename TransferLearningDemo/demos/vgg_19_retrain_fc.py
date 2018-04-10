4922e47f23f09440723aefc89e294ac6ae806127
Jason Rigby <jason.rigby@monash.edu>
2018-04-10 19:33:42 +1000

    working on demo codes

--- TransferLearningDemo/demos/vgg_19_retrain_fc.py
+++ TransferLearningDemo/demos/vgg_19_retrain_fc.py
@@ -13,47 +13,43 @@ from TransferLearningDemo.demos import IMAGENET_MEAN
 from TransferLearningDemo.utils import download, get_dirs, delete_file_safely, get_model_checkpoint
 
 
-TF_RECORDS_FILE_TRAIN = os.path.join(get_dirs()['user_cache_dir'], 'smiles_train.tfrecords')
-TF_RECORDS_FILE_TEST = os.path.join(get_dirs()['user_cache_dir'], 'smiles_test.tfrecords')
+TF_RECORDS_FILE_TRAIN = os.path.join(get_dirs()['user_cache_dir'], 'retina_train.tfrecords')
+TF_RECORDS_FILE_TEST = os.path.join(get_dirs()['user_cache_dir'], 'retina_test.tfrecords')
 
 
 if not os.path.isfile(TF_RECORDS_FILE_TRAIN) or not os.path.isfile(TF_RECORDS_FILE_TEST):
-    non_smiles = download(
-        "https://data.mendeley.com/datasets/yz4v8tb3tp/5/files/7274bb3d-ab04-4656-8188-3d2218d248ca/NON-SMILE_list.txt?dl=1",
-        target_file_name="non_smile_labels.txt")
-    smiles = download(
-        "https://data.mendeley.com/datasets/yz4v8tb3tp/5/files/72b9b8f7-72be-4559-b730-29c41faad4d9/SMILE_list.txt?dl=1",
-        target_file_name="smile_labels.txt")
-    image_zip = download("http://conradsanderson.id.au/lfwcrop/lfwcrop_color.zip")
-
-    all_data = []
-    with open(non_smiles, 'r') as f:
-        non_smiles = [re.sub(r'\.jpg', '.ppm', line.strip()) for line in f.readlines() if len(line.strip()) > 0]
-        all_data.extend([(file_name, np.array([1, 0])) for file_name in non_smiles])
-
-    with open(smiles, 'r') as f:
-        smiles = [re.sub(r'\.jpg', '.ppm', line.strip()) for line in f.readlines() if len(line.strip()) > 0]
-        all_data.extend([(file_name, np.array([0, 1])) for file_name in non_smiles])
-
-    random.shuffle(all_data)
-
-    train_data = all_data[:-100]
-    test_data = all_data[-100:]
+    annotations = download("http://cecas.clemson.edu/~ahoover/stare/manifestations/annotations.zip")
+    image_zip = download("http://cecas.clemson.edu/~ahoover/stare/images/all-images.zip")
 
     try:
-        for TF_RECORDS_FILE, data in [(TF_RECORDS_FILE_TRAIN, train_data), (TF_RECORDS_FILE_TEST, test_data)]:
-            with tf.python_io.TFRecordWriter(TF_RECORDS_FILE) as writer:
-                with zipfile.ZipFile(image_zip, 'r') as image_file_archive:
-                    for file_name, label in data:
-                        with image_file_archive.open("lfwcrop_color/faces/%s" % file_name) as image_file:
-                            img = np.array(Image.open(image_file)).reshape((64 * 64 * 3))
-                            example = tf.train.Example(features=tf.train.Features(
-                                feature={
-                                    'image': tf.train.Feature(int64_list=tf.train.Int64List(value=img)),
-                                    'label': tf.train.Feature(int64_list=tf.train.Int64List(value=label))
-                                }
-                            ))
-                            writer.write(example.SerializeToString())
+        with zipfile.ZipFile(image_zip) as image_file_archive:
+            with zipfile.ZipFile(annotations) as annotations_file_archive:
+                imgs = image_file_archive.namelist()
+                random.shuffle(imgs)
+                train_imgs = imgs[:-50]
+                test_imgs = imgs[-50:]
+                for TF_RECORDS_FILE, img_files in [(TF_RECORDS_FILE_TRAIN, train_imgs), (TF_RECORDS_FILE_TEST, test_imgs)]:
+                    with tf.python_io.TFRecordWriter(TF_RECORDS_FILE) as writer:
+                        for file_name in img_files:
+                            annotation_txt_file = "%s.fea.mg.txt" % file_name.split("/")[-1].split('.')[0]
+                            try:
+                                with annotations_file_archive.open(annotation_txt_file) as annotation_txt:
+                                    label = annotation_txt.read().decode('ascii')[11] == "1"
+                                    if label:
+                                        label = np.array([1,0])
+                                    else:
+                                        label = np.array([0,1])
+                                with image_file_archive.open(file_name) as image_file:
+                                    img = np.array(Image.open(image_file).resize((224, 224))).reshape((224 * 224 * 3))
+                                    example = tf.train.Example(features=tf.train.Features(
+                                        feature={
+                                            'image': tf.train.Feature(int64_list=tf.train.Int64List(value=img)),
+                                            'label': tf.train.Feature(int64_list=tf.train.Int64List(value=label))
+                                        }
+                                    ))
+                                    writer.write(example.SerializeToString())
+                            except KeyError:
+                                pass
     except Exception:
         delete_file_safely(TF_RECORDS_FILE_TRAIN)
         delete_file_safely(TF_RECORDS_FILE_TEST)
@@ -67,12 +63,11 @@ def input_fn(test=False, batch_size=100, epochs=1):
         if not test:
             dataset = dataset.shuffle(100)
         dataset = dataset.map(lambda example: tf.parse_single_example(example, features={
-            'image': tf.FixedLenFeature((64 * 64 * 3,), tf.int64),
+            'image': tf.FixedLenFeature((224 * 224 * 3,), tf.int64),
             'label': tf.FixedLenFeature((2,), tf.int64)
         }))
-        dataset = dataset.map(lambda example: (tf.reshape(example['image'], (64, 64, 3)), example['label']))
+        dataset = dataset.map(lambda example: (tf.reshape(example['image'], (224, 224, 3)), example['label']))
         dataset = dataset.batch(batch_size)
-        dataset = dataset.map(lambda images, labels: (tf.image.resize_images(images, (224, 224)), labels))
         return dataset.make_one_shot_iterator().get_next()
 
     return _input_fn
@@ -80,7 +75,7 @@ def input_fn(test=False, batch_size=100, epochs=1):
 
 def model_fn(features, labels, mode):
     img_mean = tf.reshape(tf.constant(IMAGENET_MEAN), (1, 1, 3))
-    output = vgg.vgg_19(features - img_mean, is_training=(mode == tf.estimator.ModeKeys.TRAIN))
+    output = vgg.vgg_19(tf.cast(features, tf.float32) - img_mean, is_training=(mode == tf.estimator.ModeKeys.TRAIN))
     logits = tf.layers.dense(tf.layers.flatten(output[1]['vgg_19/fc7']), 2, activation=None, name="new_logits")
     loss = tf.losses.softmax_cross_entropy(labels, logits)
     probabilities = tf.nn.softmax(logits)
@@ -125,7 +120,7 @@ ws = WarmStartSettings(
     vars_to_warm_start="vgg_19/.*"
 )
 
-model_dir = os.path.join(get_dirs()['user_cache_dir'], 'face_model')
+model_dir = os.path.join(get_dirs()['user_cache_dir'], 'retina_model')
 if not os.path.isdir(model_dir):
     os.makedirs(model_dir)
 estimator = tf.estimator.Estimator(model_fn, warm_start_from=ws, model_dir=model_dir)
